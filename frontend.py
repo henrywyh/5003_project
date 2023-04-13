0b6bd9933c8d52ac1892ffe51a2cdb2e2f1f1997
henrywyh <yanhowong20@hotmail.com>
2023-04-13 14:47:25 +0800

    added frontend to it

---
+++ frontend.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+
+st.set_page_config(page_title='Mental Health Analysis', page_icon=':bar_chart:', layout='wide')
+# Listen to the Enter key when the submit button is focused
+
+st.title('Mental Health Analysis')
+with st.form('my_form'):
+    text =st.text_input('Enter the text to be analyzed')
+    submit = st.form_submit_button('Submit',use_container_width=True,)
+    
+    
+if submit:
+    # call the backend
+    with st.spinner('Analyzing...'):
+        response = requests.post('http://localhost:5000/depressed', json={'text': text}).json()
+        if response == 1.0:
+            st.success('it is depression')
+        else:
+            st.success('it is not depression')
+                
